61ea6fcaf061c10e69865dbacc69c573070bf9bd
Jon Sheafe <msckj@yahoo.com>
2016-10-19 23:12:30 -0600

    creation of pwp milestone 1

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # my-pwp-jonsheafe
 creation of mypwpjonsheafe
+
+This project is for my pwp of my personal website which will discribe my personal website.
--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -2,9 +2,30 @@
 <html lang="en">
 	<head>
 		<meta charset="utf-8"/>
-		<title>Basic HTML Page</title>
+		<title>My PWP Jon Sheafe</title>
 	</head>
 	<body>
-		<h1>Hello, World! :D</h1>
+		<h1>Personal Website Project</h1>
+			<h2> Purpose</h2>
+				<p>The purpose of my PWP will to to high light my technical skills in website design and management. I will also like to advertise my services to potential employers or freelance customers.</p>
+			<h2>Audience</h2>
+				<p>My intended audience will be potential employers looking for someone who can do web, database, or coding.</p>
+			<h2>Goal</h2>
+				<p>I want to acquire full or part-time employment or an internship in the field of webdevelopment of database creation and management. My fall back goal will be to acquire freelance clients.</p>
+		<h2>Persona</h2>
+			<dl>
+				<dt>Name:</dt>
+				<dd>James Smith</dd>
+				<dt>Age:</dt>
+				<dd>52</dd>
+				<dt>Job:</dt>
+				<dd>Web Developer</dd>
+				<dt>Technology Employed</dt>
+				<dd>Smith owns an Apple computer which latest operation system. Smith also owns development software which he uses to create websites for retail clients.</dd>
+				<dt>Goal in regards to my site:</dt>
+				<dd>Smith uses my site to determine my technical skills by examining the color theme used on site. He also uses dev tools on browser to examine my coding to determine readability of code.</dd>
+			</dl>
+		<h1>Use Case</h1>
+		<p>James is looking to take on a new employee to assist him in web development for employees. James is located in Albuquerque New Mexico. He has a 2 year old computer he uses to develope websites. James is going through job applications he listed on Craigslist. He comes across Jon Sheafe's application which list his personal website. James goes to website to determine if Jon has clean and readable code which is important when working with another developer. He also wants to see how the front end of the website looks. For example, is the website professional looking or does it look like a tacky myspace site.</p>
 	</body>
 </html>
