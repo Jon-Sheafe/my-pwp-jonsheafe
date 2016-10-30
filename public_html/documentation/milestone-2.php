974addcc8b9ecb66f40e7b31c1113406c56ee0f1
Jon Sheafe <msckj@yahoo.com>
2016-10-29 21:23:51 -0600

    creation of pwp milestone 1

---
+++ public_html/documentation/milestone-2.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8"/>
+		<title>Basic HTML Page</title>
+	</head>
+	<body>
+		<h1>Content Strategy</h1>
+		<p></p>
+	</body>
+</html>
