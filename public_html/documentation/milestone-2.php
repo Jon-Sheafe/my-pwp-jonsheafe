ebeabc643ddcb466f1afad9fefc2596890761b6b
Jon Sheafe <msckj@yahoo.com>
2016-10-29 22:00:42 -0600

    creation of pwp milestone 1

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -5,7 +5,16 @@
 		<title>Basic HTML Page</title>
 	</head>
 	<body>
-		<h1>Content Strategy</h1>
-		<p></p>
+		<section>
+			<h1>Content Strategy</h1>
+			<p></p>
+		</section>
+		<section>
+			<h1>Wireframes</h1>
+			<h2>Desktop Wireframe</h2>
+
+			<h2>Mobile Wireframe</h2>
+
+		</section>
 	</body>
 </html>
