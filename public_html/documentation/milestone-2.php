30407528013b32b70a11c3abad82cdfecf5322b8
Jon Sheafe <msckj@yahoo.com>
2016-10-30 14:29:33 -0600

    creation of pwp milestone 1

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -6,10 +6,12 @@
 	</head>
 	<body>
 		<section>
+			<!-- discuss what this website will be about -->
 			<h1>Content Strategy</h1>
-			<p></p>
+			<p>PWP for jsheafe.com will be a website that discussess my technical skills in web-development. The website will have my bio that highlights my previous experience and technical skills. The second page of the website will discuss the techical skills that were mentioned on my home page, and what they are capable of; such as, PHP is a server side language that is used to create funtionality to a website. The last page will be a contact page where users can enter text into fields and contact me.</p>
 		</section>
 		<section>
+			<!-- this section will show two images of wireframes -->
 			<h1>Wireframes</h1>
 			<h2>Desktop Wireframe</h2>
 
