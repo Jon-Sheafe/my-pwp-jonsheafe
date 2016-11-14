bce12fb9e1864b7f401247fd5def058cac65cb23
Jon Sheafe <msckj@yahoo.com>
2016-11-14 13:22:14 -0700

    templetes

--- php/head.php
+++
@@ -1,3 +0,0 @@
-<?php
-
-$bootstrap = "html 5";
---
+++ public_html/index.php
@@ -0,0 +1,19 @@
+<?php
+/*grab current directory*/
+$CURRENT_DIR = __DIR__;
+
+/*set page title here*/
+$PAGE_TITLE = "Jon Sheafe";
+
+/*load head-utils.php*/
+require_once("php/templates/head-utils.php");
+?>
+
+<!-- HTML/PAGE CONTENT GOES HERE -->
+<body>
+	<header></header>
+	<main></main>
+	<footer></footer>
+
+
+</body>
