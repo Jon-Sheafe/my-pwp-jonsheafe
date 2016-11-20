56261fa311eb6c1dcc1a1c5ea20040f3558eee72
Jon Sheafe <msckj@yahoo.com>
2016-11-20 14:00:19 -0700

    added images and updated index

--- public_html/index.php
+++ public_html/index.php
@@ -12,22 +12,78 @@
 
 
 		<!-- echo the $PAGE_TITLE variable here -->
-		<title></title>
+		<title>Jon Sheafe</title>
 	</head>
 <body>
-	<header></header>
+	<header>
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-2 col-sm-offset-3">
+					<!--Home button -->
+					<button type="button" class="btn btn-info btn-lg" id="btn-home">Home</button>
+				</div>
+				<div class="col-sm-2">
+					<!-- Tech Button -->
+					<button type="button" class="btn btn-info btn-lg" id="btn-tech">Tech Disc</button>
+				</div>
+				<div class="col-sm-2">
+					<!-- Contact Button -->
+					<button type="button" class="btn btn-info btn-lg" id="btn-contact">Contact</button>
+				</div>
+			</div>
+		</div>
+	</header>
 	<main>
 		<div class="container">
 			<div class="row" >
-				<div class="col-sm-3"><img src="img/headShot.png" rel="portrait" class="img-rounded .img-responsive" ></div>
+				<div class="col-sm-3"><img src="img/headShot.png" rel="portrait" class="img-rounded img-responsive" ></div>
 				<div class="col-sm-offset-4">
 					<h2>Welcome to my personal page</h2><br />
 					<p> I am pursuing a new career in web development because I discovered that I have a passion to problem solve and find solutions using technology and process improvements. At my previous position, I realized that there were limitations to the reports and databases used in the department. I then began learning on my own and improved those databases and reports to better fit my department. However, the databases were limited as they were all in Microsoft access, which they could easily be deleted or changed. I then realize that I needed additional skills to make them more secure and resilient. This is the reason I decided to return to school and learn current computer languages and techniques. As a developer, I am able to use different languages, frameworks, and dev tools to find solutions to complex problems without having the same limitations that I had in the past.  </p>
 				</div>
 			</div>
-		</div>
+			<div class="row">
+				<div class="col-sm-1">
+					<a href="https://en.wikipedia.org/wiki/PHP" target="_blank"> <img src="img/php.png" rel="php 7" class="img-responsive" ></a>
+				</div>
+				<div class="col-sm-1"><a href="https://en.wikipedia.org/wiki/mysql" target="_blank">
+					<img src="img/mySql.png" rel="mySQL" class="img-responsive" ></a>
+				</div>
+				<div class="col-sm-1">
+					<a href="https://en.wikipedia.org/wiki/AngularJS#Angular_2" target="_blank">
+						<img src="img/angular2.jpg" rel="angular 2" class="img-rounded img-responsive" >
+					</a>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-sm-1">
+					<a href="https://en.wikipedia.org/wiki/HTML5" target="_blank">
+						<img src="img/html5.png" rel="HTML 5" class="img-responsive" >
+					</a>
+				</div>
+				<div class="col-sm-1">
+					<a href="https://en.wikipedia.org/wiki/Cascading_Style_Sheets#CSS_3" target="_blank">
+						<img src="img/css3.png" rel="CSS 3" class="img-responsive" >
+					</a>
+				</div>
+				<div class="col-sm-1">
+					<a href="https://en.wikipedia.org/wiki/JavaScript" target="_blank">
+						<img src="img/javaScript.png" rel="javaScript" class="img-rounded img-responsive" >
+					</a>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-sm-1">
+					<a href="https://en.wikipedia.org/wiki/Node.js" target="_blank">
+						<img src="img/nodeJs.png" rel="nodeJs" class="img-responsive" >
+					</a>
+				</div>
+			</div>
+			</div>
 	</main>
-	<footer></footer>
+	<footer>
+
+	</footer>
 </body>
 
 	</html>
