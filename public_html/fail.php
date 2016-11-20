a97a0438e5a029148553d88a94720bfb0ffcb9ac
Jon Sheafe <msckj@yahoo.com>
2016-11-19 18:37:14 -0700

    index update

---
+++ public_html/fail.php
@@ -0,0 +1,26 @@
+<html>
+<?php
+/*grab current directory*/
+$CURRENT_DIR = __DIR__;
+
+/*set page title here*/
+$PAGE_TITLE = "Jon Sheafe";
+
+/*load head-utils.php*/
+require_once(__DIR__ . "templates/head-utils.php");
+?>
+
+<!-- HTML/PAGE CONTENT GOES HERE -->
+<body>
+	<header>
+		<div></div>
+	</header>
+	<main>
+
+	</main>
+	<footer>
+
+	</footer>
+
+
+</body></html>
--- public_html/index.php
+++ public_html/index.php
@@ -1,26 +1,33 @@
+<!DOCTYPE html>
 <html>
-<?php
-/*grab current directory*/
-$CURRENT_DIR = __DIR__;
+	<head lang="en">
+		<!-- The 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+		<meta charset="utf-8"/>
+		<meta http-equiv="X-UA-COMPATIBLE" content="IE=edge"/>
+		<meta name="viewport" content="width=device-width, initial-scale=1"/>
 
-/*set page title here*/
-$PAGE_TITLE = "Jon Sheafe";
+		<!-- Bootstrap: Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+		<!-- Latest compiled and minified CSS -->
 
-/*load head-utils.php*/
-require_once(__DIR__ . 'php/templates/head-utils.php');
-?>
 
-<!-- HTML/PAGE CONTENT GOES HERE -->
+		<!-- echo the $PAGE_TITLE variable here -->
+		<title></title>
+	</head>
 <body>
-	<header>
-		<div></div>
-	</header>
+	<header></header>
 	<main>
-
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-3"><img src="img/headShot.png" rel="portrait" class="img-rounded .img-responsive" ></div>
+				<div class="col-sm-offset-4">
+					<h2>Welcome to my personal page</h2><br />
+					<p>test about me vvvvvvvvvvvvvvvvvvvv vvvvvvvvvvvvvvvvvvv vvvvvvvvvvvv vvvvvvvvvv</p>
+				</div>
+			</div>
+		</div>
 	</main>
-	<footer>
+	<footer></footer>
+</body>
 
-	</footer>
-
-
-</body></html>
+	</html>
