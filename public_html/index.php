12081d9ed44923e4d4007db9589f0809f08fbc6b
Jon Sheafe <msckj@yahoo.com>
2016-11-27 21:25:37 -0700

    add second nav bar

--- public_html/index.php
+++ public_html/index.php
@@ -7,7 +7,8 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1"/>
 
 		<!-- Bootstrap: Latest compiled and minified CSS -->
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+				integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 		<!-- CSS -->
 		<link href="https://fonts.googleapis.com/css?family=Dosis" rel="stylesheet">
 		<link rel="stylesheet" href="css/style.css">
@@ -16,86 +17,110 @@
 		<!-- echo the $PAGE_TITLE variable here -->
 		<title>Jon Sheafe</title>
 	</head>
-<body>
-	<header>
-		<div class="container" id="home">
-			<div class="row">
-				<div class="col-sm-2 col-sm-offset-4">
-					<!--Home button -->
-					<button type="button" class="btn btn-primary btn-lg" id="btn-home" >Home</button>
-		</div>
-				<div class="col-sm-2">
-					<!-- Tech Button -->
-					<button type="button" class="btn btn-primary btn-lg" id="btn-tech" >Tech Disc</button>
-				</div>
-				<div class="col-sm-2">
-					<!-- Contact Button -->
-					<button type="button" class="btn btn-primary btn-lg" id="btn-contact" >Contact</button>
-				</div>
-			</div>
-		</div>
-	</header>
-	<main>
-		<div class="container">
-			<div class="row" >
-				<div class="col-sm-3"><img src="img/headShot.png" rel="portrait" class="img-rounded img-responsive" ></div>
-				<div class="col-sm-offset-4">
-					<h2>Welcome to my personal page</h2><br />
-					<p> I am pursuing a new career in web development because I discovered that I have a passion to problem solve and find solutions using technology and process improvements. At my previous position, I realized that there were limitations to the reports and databases used in the department. I then began learning on my own and improved those databases and reports to better fit my department. However, the databases were limited as they were all in Microsoft access, which they could easily be deleted or changed. I then realize that I needed additional skills to make them more secure and resilient. This is the reason I decided to return to school and learn current computer languages and techniques. As a developer, I am able to use different languages, frameworks, and dev tools to find solutions to complex problems without having the same limitations that I had in the past.  </p>
+	<body>
+		<header>
+			<div class="container" id="home">
+				<div class="row">
+					<div class="col-sm-2 col-sm-offset-4">
+						<!--Home button -->
+						<button type="button" class="btn btn-primary btn-lg" id="btn-home">Home</button>
+					</div>
+					<div class="col-sm-2">
+						<!-- Tech Button -->
+						<button type="button" class="btn btn-primary btn-lg" id="btn-tech">Tech Disc</button>
+					</div>
+					<div class="col-sm-2">
+						<!-- Contact Button -->
+						<button type="button" class="btn btn-primary btn-lg" id="btn-contact">Contact</button>
+					</div>
 				</div>
 			</div>
-			<div class="row">
-				<div class="col-sm-1">
-					<a href="https://en.wikipedia.org/wiki/PHP" target="_blank"> <img src="img/php.png" rel="php 7" class="img-responsive" ></a>
+		</header>
+		<main>
+			<div class="container">
+				<div class="row">
+					<div class="col-sm-3"><img src="img/headShot.png" rel="portrait" class="img-rounded img-responsive">
+					</div>
+					<div class="col-sm-offset-4">
+						<h2>Welcome to my personal page</h2><br/>
+						<p> I am pursuing a new career in web development because I discovered that I have a passion to
+							problem solve and find solutions using technology and process improvements. At my previous
+							position, I realized that there were limitations to the reports and databases used in the
+							department. I then began learning on my own and improved those databases and reports to better fit
+							my department. However, the databases were limited as they were all in Microsoft access, which they
+							could easily be deleted or changed. I then realize that I needed additional skills to make them
+							more secure and resilient. This is the reason I decided to return to school and learn current
+							computer languages and techniques. As a developer, I am able to use different languages,
+							frameworks, and dev tools to find solutions to complex problems without having the same limitations
+							that I had in the past. </p>
+					</div>
 				</div>
-				<div class="col-sm-1"><a href="https://en.wikipedia.org/wiki/mysql" target="_blank">
-					<img src="img/mySql.png" rel="mySQL" class="img-responsive" ></a>
-				</div>
-				<div class="col-sm-1">
-					<a href="https://en.wikipedia.org/wiki/AngularJS#Angular_2" target="_blank">
-						<img src="img/angular2.jpg" rel="angular 2" class="img-rounded img-responsive" >
-					</a>
-				</div>
-			</div>
-			<div class="row">
-				<div class="col-sm-1">
-					<a href="https://en.wikipedia.org/wiki/HTML5" target="_blank">
-						<img src="img/html5.png" rel="HTML 5" class="img-responsive" >
-					</a>
-				</div>
-				<div class="col-sm-1">
-					<a href="https://en.wikipedia.org/wiki/Cascading_Style_Sheets#CSS_3" target="_blank">
-						<img src="img/css3.png" rel="CSS 3" class="img-responsive" >
-					</a>
-				</div>
-				<div class="col-sm-1">
-					<a href="https://en.wikipedia.org/wiki/JavaScript" target="_blank">
-						<img src="img/javaScript.png" rel="javaScript" class="img-rounded img-responsive" >
-					</a>
-				</div>
-			</div>
-			<div class="row">
-				<div class="col-sm-1">
-					<a href="https://en.wikipedia.org/wiki/Node.js" target="_blank">
-						<img src="img/nodeJs.png" rel="nodeJs" class="img-responsive" >
-					</a>
-				</div>
-			</div>index.php
-		</div>
-		<div class="container " id="tech" >
-			<span >
-				<div class="col-sm-9 col-sm-offset-3" id="tech-boarder">
-					<h2>Tech Details</h2>
-					<p><b>Php: </b> PHP is a server side language that is used to give websites. <br />
-						<b>HTML 5:</b>
+
+				<div class="container " id="tech">
+
+					<div class="col-sm-9 col-sm-offset-3" id="tech-boarder">
+						<h2>Tech Details</h2>
+						<div class="row">
+							<div class="col-sm-1">
+								<a href="https://en.wikipedia.org/wiki/PHP" target="_blank"> <img src="img/php.png" rel="php 7" class="img-responsive"></a>
+							</div>
+							<div class="col-sm-1"><a href="https://en.wikipedia.org/wiki/mysql" target="_blank">
+									<img src="img/mySql.png" rel="mySQL" class="img-responsive"></a>
+							</div>
+							<div class="col-sm-1">
+								<a href="https://en.wikipedia.org/wiki/AngularJS#Angular_2" target="_blank">
+									<img src="img/angular2.jpg" rel="angular 2" class="img-rounded img-responsive">
+								</a>
+							</div>
+						</div>
+						<div class="row">
+							<div class="col-sm-2">
+								<a href="https://en.wikipedia.org/wiki/HTML5" target="_blank">
+									<img src="img/html5.png" rel="HTML 5" class="img-responsive">
+								</a>
+							</div>
+							<div class="col-sm-2">
+								<a href="https://en.wikipedia.org/wiki/Cascading_Style_Sheets#CSS_3" target="_blank">
+									<img src="img/css3.png" rel="CSS 3" class="img-responsive">
+								</a>
+							</div>
+							<div class="col-sm-2">
+								<a href="https://en.wikipedia.org/wiki/JavaScript" target="_blank">
+									<img src="img/javaScript.png" rel="javaScript" class="img-rounded img-responsive">
+								</a>
+							</div>
+						</div>
+						<div class="row">
+							<div class="col-sm-1">
+								<a href="https://en.wikipedia.org/wiki/Node.js" target="_blank">
+									<img src="img/nodeJs.png" rel="nodeJs" class="img-responsive">
+								</a>
+							</div>
+						</div>
+						index.php
+					</div>
 					</p>
 				</div>
-			</span>
-		</div>
-	</main>
-	<footer>
+			</div>
+		</main>
+		<footer>
+			<div class="container"
+				  <div class="row">
+					  <div class="footer navbar-fixed-bottom">
+						  <!--Home button -->
+						  <button type="button" class="btn btn-primary btn-lg" id="btn-home">Home</button>
+					  </div>
+					  <div class="col-sm-2">
+						  <!-- Tech Button -->
+						  <button type="button" class="btn btn-primary btn-lg" id="btn-tech">Tech Disc</button>
+					  </div>
+					  <div class="col-sm-2">
+						  <!-- Contact Button -->
+						  <button type="button" class="btn btn-primary btn-lg" id="btn-contact">Contact</button>
+					  </div>
+				  </div>
 
-	</footer>
-</body>
+		</footer>
+	</body>
 
-	</html>
+</html>
